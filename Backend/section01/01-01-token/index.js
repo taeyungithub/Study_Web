a04ea96d1ef1806d82d99c990bbe886f021a0ee7
taeyungithub <rlaxodbs629@naver.com>
2024-06-19 07:44:39 +0900

    24-06-19

--- Backend/section01/01-01-token/index.js
+++
@@ -1,8 +0,0 @@
-function getToken() {
-  const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0"); //임의의 숫자생성
-  //String, padStart를 활용하여 사라진 0을 삽입
-  console.log(result);
-}
-
-//실제 실행
-getToken();
